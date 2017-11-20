51b35a3de04570fe3c6e1d49ecbcf698e12c4b5f
Brian Clark <blclark4@cox.net>
2017-11-20 13:08:10 -0600

    Added some line break handling

--- TextVisualizer.py
+++ TextVisualizer.py
@@ -3,7 +3,6 @@ import PIL.Image
 import PIL.ImageFont
 import PIL.ImageOps
 import PIL.ImageDraw
-from Oracle import Oracle
 
 # I'm considering this as a better way of dealing with long messages
 # perhaps in conjunction with the threaded tweets.
@@ -15,20 +14,23 @@ def text_image_from_file(text_path, font_path=None):
     with open(text_path) as text_file:
         lines = tuple(l.rstrip() for l in text_file.readlines())
 
-
-    total_length = sum([len(l) for l in lines])
-    char_width = 60
-    sequence = []
-    print(lines)
-    for line in lines:
-        sequence.extend(Oracle.split_by_width(line, char_width, False))
-        sequence.append(" ")
+    sequence = arrange_text(lines)
 
     lines = tuple(sequence)
-    print(lines)
-    grayscale = 'L'
+    # print(lines)
+    return render_text(font_path, lines)
 
-    large_font = 20
+def image_file_path_from_text(source_text, font_path=None, image_path='render_text.png'):
+    lines = tuple(l.rstrip() for l in source_text.split('\n'))
+    sequence = arrange_text(lines)
+    lines = tuple(sequence)
+    img = render_text(font_path, lines)
+    img.save(image_path)
+    return image_path
+
+def render_text(font_path, lines):
+    grayscale = 'L'
+    large_font = 12
     font_path = font_path or 'cour.ttf'
     try:
         font = PIL.ImageFont.truetype(font_path, size=large_font)
@@ -41,13 +43,12 @@ def text_image_from_file(text_path, font_path=None):
     max_width_line = max(lines, key=lambda s: font.getsize(s)[0])
     # max height is adjusted down because it's too large visually for spacing
     test_string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    max_height =  pt2px(font.getsize(test_string)[1])
+    max_height = pt2px(font.getsize(test_string)[1])
     max_width = pt2px(font.getsize(max_width_line)[0])
-    height = max_height * len(lines)    # perfect or a little oversized
+    height = max_height * len(lines)  # perfect or a little oversized
     width = int(round(max_width + 40))  # a little oversized
     image = PIL.Image.new(grayscale, (width, height), color=PIXEL_OFF)
     draw = PIL.ImageDraw.Draw(image)
-
     # draw each line of text
     vertical_position = 5
     horizontal_position = 5
@@ -62,9 +63,64 @@ def text_image_from_file(text_path, font_path=None):
     image = image.crop(c_box)
     return image
 
-def text_image_usage():
-    img = text_image_from_file('render_test.txt')
-    img.show()
-    img.save('render_test.png')
 
-text_image_usage()
+def arrange_text(lines):
+    total_length = sum([len(l) for l in lines])
+    char_width = int(total_length * 0.1111 + 20)
+    if char_width < 30:
+        char_width = 30
+    elif char_width > 60:
+        char_width = 60
+    sequence = []
+    # print(lines)
+    for line in lines:
+        sequence.extend(split_by_width(line, char_width, False))
+        sequence.append(" ")
+    return sequence
+
+
+def split_by_width(src_text, allow_length, try_balance=True):
+    parts = []
+    partsizes = []
+    words = src_text.split()
+    if try_balance:
+        midword = int(len(words) / 2)
+        parts.append(words[:midword])
+        partsizes.append(sum([len(w) for w in parts[0]]) + len(parts[0]))
+        parts.append(words[midword:])
+        partsizes.append(sum([len(w) for w in parts[1]]) + len(parts[1]))
+    else:
+        parts.append(words)
+        partsizes.append(sum([len(w) for w in parts[0]]) + len(parts[0]))
+
+    while max(partsizes) > allow_length:
+        for widx in range(len(parts)):
+            if partsizes[widx] > allow_length:
+                if widx > 0 and partsizes[widx - 1] < allow_length - len(parts[widx][0]):
+                    # move to previous part
+                    move_text = parts[widx].pop(0)
+                    parts[widx - 1].append(move_text)
+                    partsizes[widx - 1] += len(move_text) + 1
+                    partsizes[widx] -= len(move_text) + 1
+                else:
+                    # move to next part
+                    if len(parts) == widx + 1:
+                        parts.append([])
+                        partsizes.append(0)
+                    move_text = parts[widx].pop()
+                    parts[widx + 1].insert(0, move_text)
+                    partsizes[widx + 1] += len(move_text) + 1
+                    partsizes[widx] -= len(move_text) + 1
+    return [" ".join(p) for p in parts]
+
+def text_image_usage():
+    text_to_show = ("I live daily with the realization that people I thought were good people are really hateful dirtbags.",
+             "It gets easier with time.")
+    arranged = arrange_text(text_to_show)
+    img = render_text(font_path=None, lines=arranged)
+
+    img.show()
+    # img.save('render_test.png')
+
+if __name__ == "__main__":
+    text_image_usage()
